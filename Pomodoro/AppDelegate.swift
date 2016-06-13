ca1c7a87f4c6597d42594fab2437d965dbf3abeb
Alberto Quesada <qa.alberto@gmail.com>
2016-06-13 11:31:15 +0200

    Initial Commit

---
+++ Pomodoro/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Pomodoro
+//
+//  Created by Alberto Quesada Aranda on 13/6/16.
+//  Copyright © 2016 Alberto Quesada Aranda. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
