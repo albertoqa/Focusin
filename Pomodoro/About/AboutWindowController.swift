ed3cfd114305840d9dfc539bca6263411b27add8
Alberto Quesada <qa.alberto@gmail.com>
2016-06-14 17:00:04 +0200

    About screen.

--- Pomodoro/About/AboutWindowController.swift
+++ Pomodoro/About/AboutWindowController.swift
@@ -12,8 +12,13 @@ class AboutWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        self.window?.center()
+        self.window?.makeKeyAndOrderFront(nil)
+        NSApp.activateIgnoringOtherApps(true)
+    }
+    
+    override var windowNibName : String! {
+        return "AboutWindowController"
     }
     
 }
