31500c85c61a0e038c79539206a6e3630241136b
Alberto Quesada <qa.alberto@gmail.com>
2016-06-16 11:20:59 +0200

    Project page

---
+++ web/js/app.js
@@ -0,0 +1,15 @@
+(function () {
+
+  angular.module('focusinApp', []);
+
+  angular
+    .module('focusinApp')
+    .controller('mainCtrl', mainCtrl);
+
+  mainCtrl.$inject = ['$scope', '$http', '$timeout'];
+  function mainCtrl ($scope, $http, $timeout) {
+    var vm = this;
+
+  }
+
+})();
